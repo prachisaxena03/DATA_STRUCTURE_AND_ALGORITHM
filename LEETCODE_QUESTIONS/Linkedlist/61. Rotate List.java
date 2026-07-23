2517ed0deb92d0887834a7615f418a7ac195c0da
prachisaxena03 <prachisaxena9520@gmail.com>
2026-07-23 20:01:00 +0530

    Create 61. Rotate List.java

---
+++ LEETCODE_QUESTIONS/Linkedlist/61. Rotate List.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null||k<=0||head.next==null){
+            return head;
+        }
+        ListNode tail=head;
+        int length=1;
+        while(tail.next!=null){
+            tail=tail.next;
+            length++;
+        }
+        k=k%length;
+        if(k==0){
+            return head;
+        }
+        tail.next=head;
+        ListNode newtail=head;
+        for(int i=0;i<length-k-1;i++){
+            newtail=newtail.next;
+        }
+        ListNode newHead=newtail.next;
+        newtail.next=null;
+        return newHead;
+
+    }
+}
